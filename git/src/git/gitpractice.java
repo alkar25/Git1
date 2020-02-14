bc22f4344fb36f2962bb056eaff25da5bd41e54b
alkar <mattgold2525@gmail.com>
2020-02-13 20:54:28 -0500

    branch commit

--- git/src/git/gitpractice.java
+++ git/src/git/gitpractice.java
@@ -3,12 +3,12 @@ package git;
 public class gitpractice {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("1st change");
-		
+
 		System.out.println("1st change online");
 		System.out.println("second change");
-
+		System.out.println("branch");
 	}
 
 }
